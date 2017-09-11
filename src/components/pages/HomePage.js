4458663a8ff25e582a968bbf1fedc614b8349ac5
zazk <juandedioz@gmail.com>
2017-09-11 11:52:23 -0500

    Added Router

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router-dom'; 
+
+const HomePage = () =>(
+    <div>
+        <Link to="/login">Login </Link>
+    </div>
+);
+
+export default HomePage;
---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const LoginPage = () =>(
+    <div>
+        <h1>Login Page</h1>
+        <Link to="/">Back to HomePage</Link>
+    </div>
+);
+
+export default LoginPage
