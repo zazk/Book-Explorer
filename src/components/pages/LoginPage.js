c83383f0818a9edf3aabb40eace62f132f36e306
zazk <juandedioz@gmail.com>
2017-09-12 00:44:26 -0500

    Added Form Component

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import LoginForm from '../forms/LoginForm'; 
 
 const LoginPage = () =>(
     <div>
         <h1>Login Page</h1>
         <Link to="/">Back to HomePage</Link>
+        <LoginForm />
     </div>
 );
 
