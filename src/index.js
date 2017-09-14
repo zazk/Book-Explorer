3b7e6e54ddab702aa07efc6884e93eebe46201dc
zazk <juandedioz@gmail.com>
2017-09-13 23:26:35 -0500

    Add Redux and Middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddlewere } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
+import "semantic-ui-css/semantic.min.css";
+
+const store = createStore(rootReducer, applyMiddlewere(thunk));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 registerServiceWorker();
