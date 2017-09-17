bb0451ff8cbd0e80008742080d69b914e3bc475e
zazk <juandedioz@gmail.com>
2017-09-17 13:41:53 -0500

    Added Redux, Reducers and API Promise

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import LoginForm from "../forms/LoginForm";
+import { login } from "../../actions/auth";
 
 class LoginPage extends React.Component {
-    submit = data => {
-        console.log(data);
-    };
+    submit = data =>
+        this.props.login(data).then(() => this.props.history.push("/"));
     render() {
         return (
             <div>
@@ -17,4 +19,10 @@ class LoginPage extends React.Component {
     }
 }
 
-export default LoginPage;
+LoginPage.proptypes = {
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired
+    }).isRequired,
+    login: PropTypes.func.isRequired
+};
+export default connect(null, { login })(LoginPage);
